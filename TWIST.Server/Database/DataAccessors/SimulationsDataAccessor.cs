a331b600782a42678f2cd6b9d31a2e2fb745b45f
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-09-03 23:10:17 -0500

    T-21 Create simulations and teams data accessor

---
+++ TWIST.Server/Database/DataAccessors/SimulationsDataAccessor.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.SqlTypes;
+using TWISTServer.Database.Records;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TWISTServer.Database.DataAccessors
+{
+    public class SimulationsDataAccessor : DataAccessor
+    {
+        public SimulationsDataAccessor() { }
+
+        public IEnumerable<SimulationRecord> GetAllSimulations()
+        {
+            string sql = $"select simulation_id, name, participants, start_date, end_date, active from simulations;";
+            return Database.Query(
+                sql,
+                SimulationRecord.FromRow
+            );
+        }
+
+        public IEnumerable<SimulationRecord> GetSimulation(int simulationId)
+        {
+            string sql = @"select 
+simulation_id, name, participants, start_date, end_date, active 
+from simulations WHERE 
+simulation_id = @simulation_id;";
+            return Database.Query(
+                sql,
+                SimulationRecord.FromRow,
+                [
+                    new("@simulation_id", SqlDbType.Int) { Value = simulationId },
+                ]
+            );
+        }
+
+        public int InsertSimulation(SimulationRecord simulation)
+        {
+            string sql = @"
+INSERT INTO simulations 
+(name, participants, start_date, end_date, active) 
+VALUES 
+(@name, @participants, @start_date, @end_date, @active)";
+
+            SqlParameter[] parameters = [
+                new("@name", SqlDbType.NVarChar, -1) { Value = simulation.Name },
+                new("@participants", SqlDbType.DateTime) { Value = simulation.Participants },
+                new("@start_date", SqlDbType.DateTime, 64) { Value = simulation.StartDate },
+                new("@end_date", SqlDbType.DateTime) { Value = simulation.EndDate},
+                new("@active", SqlDbType.Bit) { Value = simulation.Active },
+            ];
+
+            return Database.NonQuery(sql, parameters);
+        }
+    }
+}
---
+++ TWIST.Server/Database/DataAccessors/TeamDataAccessor.cs
@@ -0,0 +1,101 @@
+﻿using System.Data.SqlTypes;
+using TWISTServer.Database.Records;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TWISTServer.Database.DataAccessors
+{
+    public class TeamDataAccessor : DataAccessor
+    {
+        public TeamDataAccessor() { }
+
+        public IEnumerable<TeamRecord> GetAllTeams()
+        {
+            string sql = $"select team_id, simulation_id, country, interest_group, members from teams;";
+            return Database.Query(
+                sql,
+                TeamRecord.FromRow
+            );
+        }
+
+        public IEnumerable<TeamRecord> GetTeam(int teamId)
+        {
+            string sql = @"select 
+team_id, simulation_id, country, interest_group, members 
+from teams WHERE
+team_id = @team_id;";
+            return Database.Query(
+                sql,
+                TeamRecord.FromRow,
+                [
+                    new("@team_id", SqlDbType.Int) { Value = teamId },
+                ]
+            );
+        }
+
+        public IEnumerable<TeamRecord> GetTeamBySimulation(int simulationId)
+        {
+            string sql = @"select 
+team_id, simulation_id, country, interest_group, members 
+from teams WHERE
+simulation_id = @simulation_id;";
+            return Database.Query(
+                sql,
+                TeamRecord.FromRow,
+                [
+                    new("@simulation_id", SqlDbType.Int) { Value = simulationId },
+                ]
+            );
+        }
+
+        public IEnumerable<TeamRecord> GetTeamByCountry(int country)
+        {
+            string sql = @"select 
+team_id, simulation_id, country, interest_group, members 
+from teams WHERE
+country = @country;";
+            return Database.Query(
+                sql,
+                TeamRecord.FromRow,
+                [
+                    new("@country", SqlDbType.Int) { Value = country },
+                ]
+            );
+        }
+
+        public IEnumerable<TeamRecord> GetTeamByCountryAndSimulationId(int country, int simulationId)
+        {
+            string sql = @"select 
+team_id, simulation_id, country, interest_group, members 
+from teams WHERE
+country = @country AND
+simulation_id = @simulation_id;";
+            return Database.Query(
+                sql,
+                TeamRecord.FromRow,
+                [
+                    new("@country", SqlDbType.Int) { Value = country },
+                    new("@simulation_id", SqlDbType.Int) { Value = simulationId },
+                ]
+            );
+        }
+
+        public int InsertTeam(TeamRecord team)
+        {
+            string sql = @"
+INSERT INTO teams 
+(simulation_id, country, interest_group, members) 
+VALUES 
+(@simulation_id, @country, @interest_group, @members)";
+
+            SqlParameter[] parameters = [
+                new("@simulation_id", SqlDbType.Int) { Value = team.SimulationId },
+                new("@country", SqlDbType.Int) { Value = team.Country },
+                new("@interest_group", SqlDbType.Int) { Value = team.InterestGroup },
+                new("@members", SqlDbType.NVarChar, -1) { Value = team.Members},
+            ];
+
+            return Database.NonQuery(sql, parameters);
+        }
+    }
+}
