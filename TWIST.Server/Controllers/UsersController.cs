fc6d8072d4c2c83b6fe4bdb3866ea512b7391bd6
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2024-09-03 19:17:07 -0500

    T-21 Create a controller for the Users

---
+++ TWIST.Server/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TWISTServer.Database.DataAccessors;
+using TWISTServer.Database.Records;
+
+namespace TWISTServer.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController
+    {
+        private readonly UserDataAccessor dataAccessor = new();
+
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<UserRecord> GetUsers()
+        {
+            return dataAccessor.GetAllUsers();
+        }
+
+        [HttpPut]
+        [Route("")]
+        public JsonResult Put([FromBody] UserRecord user)
+        {
+            dataAccessor.InsertUser(user);
+            return new JsonResult($"Successfully added user {user.Username}!");
+        }
+    }
+}
