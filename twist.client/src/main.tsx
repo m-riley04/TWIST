2851c2a8fd43cd63a65fc37802199b9857f91322
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-12-25 16:23:45 -0600

    add basic instructor account auth

--- twist.client/src/main.tsx
+++ twist.client/src/main.tsx
@@ -9,7 +9,10 @@ createRoot(document.getElementById('root')!).render(
         <Auth0Provider
             domain={import.meta.env.VITE_AUTH0_DOMAIN}
             clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-            authorizationParams={{ redirect_uri: window.location.origin }}
+            authorizationParams={{
+                redirect_uri: window.location.origin + "/instructor",
+                
+            }}
         >
             <App />
         </Auth0Provider>
--- twist.client/src/pages/InstructorPage/InstructorPage.tsx
+++ twist.client/src/pages/InstructorPage/InstructorPage.tsx
@@ -1,27 +1,25 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "../../components/LogoutButton";
-import LoginButton from "../../components/LoginButton";
 
 const InstructorPage = () => {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, error, isLoading, loginWithRedirect, user, getAccessTokenSilently } = useAuth0();
+
+    if (error) return <div>Oops... {error.message}</div>;
+
+    if (isLoading) return <div>Loading...</div>;
 
     // Authenticated view
     if (isAuthenticated) return ( 
         <>
             <h1>Instructor Home</h1>
+            <p><i>Account: {user?.name}</i></p>
             <p>You can control, view, and create simulations here.</p>
             <LogoutButton />
         </>
     );
 
     // Fallback login
-    return (
-        <>
-            <h1>Instructor Login</h1>
-            <p>Click the button below to be redirected to the instructor login screen.</p>
-            <LoginButton />
-        </>
-    );
+    loginWithRedirect();
 }
 
 export default InstructorPage;
