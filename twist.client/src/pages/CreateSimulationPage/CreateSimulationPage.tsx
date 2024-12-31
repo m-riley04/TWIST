f757331877188a93ea2a3285e343f5ee1f69de83
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-12-31 15:25:49 -0600

    add code generation to simulation creation

--- twist.client/src/pages/CreateSimulationPage/CreateSimulationPage.tsx
+++ twist.client/src/pages/CreateSimulationPage/CreateSimulationPage.tsx
@@ -7,9 +7,9 @@ import { Button, Form, FormLabel } from "react-bootstrap";
 const CreateSimulationPage = () => {
     const { isAuthenticated, error, isLoading, loginWithRedirect, user } = useAuth0();
 
-    function createSimulation(name: string) {
-        axios.put<SimulationModel>('https://localhost:7026/api/simulations',
-            {
+    async function createSimulation(name: string, code: string) {
+        try {
+            await axios.put<SimulationModel>("https://localhost:7026/api/simulations", {
                 name: name,
                 participants: JSON.stringify([]),
                 start_date: new Date().toISOString(),
@@ -18,19 +18,61 @@ const CreateSimulationPage = () => {
                 responses: JSON.stringify([]),
                 asks: JSON.stringify([]),
                 concessions: JSON.stringify([]),
-                round: 0
-            })
-            .then((response) => { })
-            .catch((error) => console.log(error));
+                round: 0,
+                code: code
+            });
+        } catch (error) {
+            console.log(`Unable to create simulation: ${error}`);
+        }
     }
 
-    function onCreateClicked(event: FormEvent) {
+    async function doesSimulationExist(code: string) {
+        try {
+            const response: SimulationModel[] = await axios
+                .get<SimulationModel[]>(`https://localhost:7026/api/simulations/${code}`)
+                .then(response => response.data);
+            
+            return response.length > 0;
+
+        } catch (error) {
+            console.error(`Unable to check code: ${error}`);
+            return true;
+        }
+    }
+
+    async function generateCode(timeout: number = 5): Promise<string> {
+        for (let i = 0; i < timeout; i++) {
+            const code = Math.random().toString(36).substring(7);
+            const exists = await doesSimulationExist(code);
+
+            if (!exists) return code;
+        }
+
+        throw new Error("Unable to generate unique code.");
+    }
+
+    async function onCreateClicked(event: FormEvent) {
         event.preventDefault();
+
+        // Get data from form
         const form = document.getElementById("simulation-form") as HTMLFormElement;
         const data = new FormData(form);
         const name = data.get("name") as string;
-        createSimulation(name);
-        window.location.assign("/instructor/room"); /// TODO: Redirect to the specific room
+
+        // Check and generate code
+        let code;
+        try {
+            code = await generateCode();
+        } catch (error) {
+            console.error(`Unable to create new simulation: ${error}`);
+            return;
+        }
+
+        // Create simulation
+        await createSimulation(name, code);
+
+        // Navigate to the room
+        window.location.assign(`/instructor/room/${code}`);
     }
 
     if (error) return <div>Oops... {error.message}</div>;
