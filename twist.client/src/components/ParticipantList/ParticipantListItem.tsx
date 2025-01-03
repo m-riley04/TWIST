e70f918662e77653f9e7f5d6a219cad6ac4c61f7
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2025-01-03 11:32:20 -0600

    add ParticipantList component

---
+++ twist.client/src/components/ParticipantList/ParticipantListItem.tsx
@@ -0,0 +1,20 @@
+import { Button, Dropdown, DropdownItem } from 'react-bootstrap';
+import type ParticipantModel from '../../models/ParticipantModel';
+import "./style.scss";
+
+const ROLES: string[] = ["Protectionist", "Pro-Trade", "Security"];
+const COUNTRIES: string[] = ["USA", "China"];
+
+const ParticipantListItem = ({ participant }: { participant: ParticipantModel }) => {
+    return (
+        <div className="participantListItem" >
+            <p>{participant.username}</p>
+            <p>{participant.email}</p>
+            <Dropdown>{COUNTRIES.map((c, i) => <DropdownItem key={i}>{c}</DropdownItem>)}</Dropdown>
+            <Dropdown>{ROLES.map((r, i) => <DropdownItem key={i}>{r}</DropdownItem>)}</Dropdown>
+            <Button>Remove</Button>
+        </div>
+    );
+}
+
+export default ParticipantListItem;
