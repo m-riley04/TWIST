9a521d6431134a8734d66401c0994dc8a1fb17c8
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2025-01-03 11:34:18 -0600

    add checkbox to indicate status

--- twist.client/src/components/SimulationList/SimulationListItem.tsx
+++ twist.client/src/components/SimulationList/SimulationListItem.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, FormCheck } from 'react-bootstrap';
 import type SimulationModel from '../../models/SimulationModel';
 import "./style.scss";
 
@@ -8,7 +8,7 @@ const SimulationsList = ({ simulation }: { simulation: SimulationModel }) => {
         <div className="simulationListItem" >
             <p>{simulation.name}</p>
             <p>{simulation.round}</p>
-
+            <FormCheck checked={simulation.active} style={{ pointerEvents: 'none' }} />
             <Button onClick={() => window.location.assign(`/instructor/room/${simulation.code}`)}>Open</Button>
         </div>
     );
