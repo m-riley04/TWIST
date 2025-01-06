37637acdfc788cba4f551d4b1026b7a718145fe0
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2025-01-06 14:44:39 -0600

    add simulation deletion modal

--- twist.client/src/pages/InstructorPage/InstructorPage.tsx
+++ twist.client/src/pages/InstructorPage/InstructorPage.tsx
@@ -3,14 +3,43 @@ import LogoutButton from "../../components/LogoutButton";
 import SimulationsList from "../../components/SimulationList/SimulationList";
 import { useState } from "react";
 import { useEffect } from "react";
-import axios from 'axios';
-import { Button } from "react-bootstrap";
-import { getSimulations } from "../../server/simulation_management";
+import { Button, Modal } from "react-bootstrap";
+import { deleteSimulation, getSimulations } from "../../server/simulation_management";
 import SimulationModel from "../../models/SimulationModel";
 
 const InstructorPage = () => {
     const { isAuthenticated, error, isLoading, loginWithRedirect, user } = useAuth0();
     const [simulations, setSimulations] = useState<SimulationModel[]>([]);
+    const [show, setShow] = useState(false);
+    const [selectedSimulation, setSelectedSimulation] = useState<SimulationModel>();
+    const [selectedIndex, setSelectedIndex] = useState<number>();
+
+    const handleClose = () => setShow(false);
+    const handleDeleteClicked = (sim: SimulationModel, i: number) => {
+        // Show modal
+        setShow(true);
+
+        // Get the selected simulation and index
+        setSelectedSimulation(sim);
+        setSelectedIndex(i);
+    }
+    const handleSimulationDelete = () => {
+        // Check if the selected simulation is undefined
+        if (selectedSimulation === undefined) {
+            console.error("Unable to delete simulation: No simulation selected.");
+            return;
+        }
+
+        // Delete simulation
+        deleteSimulation(selectedSimulation.code)
+            .then(() => {
+                // Remove simulation from list
+                setSimulations(simulations.filter((sim, i) => i !== selectedIndex));
+            });
+
+        // Close modal
+        handleClose();
+    }
     
     useEffect(() => {
         // Load simulations data from API
@@ -29,14 +58,28 @@ const InstructorPage = () => {
     // Authenticated view
     if (isAuthenticated) return ( 
         <>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>Confirm deletion.</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>Are you sure you want to remove this simulation?</Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleSimulationDelete}>Delete</Button>
+                    <Button variant="primary" onClick={handleClose}>Cancel</Button>
+                </Modal.Footer>
+
+            </Modal>
+
             <h1>Instructor Home</h1>
             <p><i>Account: {user?.name}</i></p>
             <p>You can control, view, and create simulations here.</p>
             <h2>Simulations</h2>
-            <SimulationsList simulations={simulations} />
+            <SimulationsList simulations={simulations} onDeleteClicked={handleDeleteClicked} />
             <Button onClick={() => { window.location.assign("/instructor/create") }}>Create New Simulation</Button>
             <LogoutButton />
-            <a href="/">Participant?</a>
+            <a href="/">Participant Home</a>
         </>
     );
 
