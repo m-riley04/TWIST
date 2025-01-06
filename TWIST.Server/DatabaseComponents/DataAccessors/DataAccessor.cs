497fa69b802862b0994987e01cf93f5eb19e956e
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2025-01-06 17:27:12 -0600

    fix DBNull value parsing

--- TWIST.Server/DatabaseComponents/DataAccessors/DataAccessor.cs
+++ TWIST.Server/DatabaseComponents/DataAccessors/DataAccessor.cs
@@ -60,17 +60,28 @@ namespace TWISTServer.DatabaseComponents.DataAccessors
                 columnNameValueDict.Add(pName, pValue);
             }
 
+            string[] columnsToInsert = T.Columns.Keys
+                .Where(c => c != PrimaryKeyColumn)
+                .ToArray();
+
             // Create the SQL text input
             string sql = $@"
 INSERT INTO {TableName} 
-({GetColumnsAsSql(T.Columns.Keys.Skip(1))}) 
-VALUES ({GetColumnsAsSql(T.Columns.Keys.Skip(1), "@")});";
+({GetColumnsAsSql(columnsToInsert)}) 
+VALUES ({GetColumnsAsSql(columnsToInsert, "@")});";
 
             // Construct the list of parameters
             List<SqlParameter> parameters = new();
-            foreach (string columnName in T.Columns.Keys)
+            foreach (string columnName in columnsToInsert)
             {
-                parameters.Add(new($"@{columnName}", T.Columns[columnName]) { Value = columnNameValueDict[columnName] });
+                object? value = columnNameValueDict[columnName];
+
+                // Convert null to DBNull.Value
+                parameters.Add(
+                    new($"@{columnName}", T.Columns[columnName]) { 
+                        Value = value ?? DBNull.Value
+                    }
+                );
             }
 
             // Call to the database
