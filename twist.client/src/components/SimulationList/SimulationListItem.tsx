772ab3a3f3bae1b9f9f55ea51a9e83b2f0fff464
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2025-01-06 14:43:05 -0600

    add dates, code, and methods

--- twist.client/src/components/SimulationList/SimulationListItem.tsx
+++ twist.client/src/components/SimulationList/SimulationListItem.tsx
@@ -2,16 +2,21 @@ import { Button, FormCheck } from 'react-bootstrap';
 import type SimulationModel from '../../models/SimulationModel';
 import "./style.scss";
 
-const SimulationsList = ({ simulation }: { simulation: SimulationModel }) => {
+const SimulationsListItem = ({ simulation, onDeleteClicked }: { simulation: SimulationModel, onDeleteClicked: ()=>void }) => {
 
     return (
         <div className="simulationListItem" >
             <p>{simulation.name}</p>
+            <p>{simulation.code}</p>
+            <p>{simulation.start_date.toDateString()}</p>
+            <p>{simulation.end_date?.toDateString()}</p>
             <p>{simulation.round}</p>
-            <FormCheck checked={simulation.active} style={{ pointerEvents: 'none' }} />
+            <FormCheck checked={simulation.active} readOnly />
             <Button onClick={() => window.location.assign(`/instructor/room/${simulation.code}`)}>Open</Button>
+            <Button>Edit</Button>
+            <Button variant="danger" onClick={onDeleteClicked}>Delete</Button>
         </div>
     );
 }
 
-export default SimulationsList;
+export default SimulationsListItem;
