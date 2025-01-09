543e5d896ed5c713e49cef6916d8c8d740144178
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2025-01-09 12:17:24 -0600

    add basic participant joining

--- TWIST.Server/Hubs/RoomHub.cs
+++ TWIST.Server/Hubs/RoomHub.cs
@@ -1,23 +1,45 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.ComponentModel.DataAnnotations;
+using TWISTServer.Controllers;
+using TWISTServer.DatabaseComponents.DataAccessors;
+using TWISTServer.DatabaseComponents.Records;
 
 namespace TWISTServer.Hubs
 {
     public interface IRoomClient
     {
-        Task ParticipantJoined(string username, string email);
+        Task ParticipantJoined(int id, string username, string email);
     }
 
     public class RoomHub : Hub<IRoomClient>
     {
+        SimulationsDataAccessor simAccessor = new();
+        ParticipantsDataAccessor partAccessor = new();
         public async Task JoinRoom(string code, string username, string email) // FYI: Methods like this will SILENTLY FAIL if you do not pass the correct param types
         {
-            /// TODO:  Check if the email address already exists
+            /// TODO: Check if the email address already exists
+            
+            // Get simulation
+            List<SimulationRecord> sims = simAccessor.GetByCode(code).ToList();
 
-            /// TODO: Add the participant
+            if (sims.Count == 0)
+            {
+                throw new HubException("Simulation not found");
+            }
+
+            SimulationRecord sim = sims.First();
+
+            /// Add the participant to participants table
+            ParticipantRecord participant = new ParticipantRecord(0, null, null, sim.SimulationId, username, email);
+            int participantId = partAccessor.InsertAndReturnId(participant);
+
+            // Add participant to simulation record
+            sim.Participants.Append(participantId);
+            simAccessor.UpdateParticipants(sim.SimulationId, sim.Participants);
 
             // Send signal to all clients
-            await Clients.All.ParticipantJoined(email, username);
+            await Clients.All.ParticipantJoined(participantId, email, username);
         }
     }
 }
