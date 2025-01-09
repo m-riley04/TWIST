b6f121fd559b8b226690c6082611047eb80c20df
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2025-01-09 17:41:07 -0600

    update JoinRoom

--- TWIST.Server/Hubs/RoomHub.cs
+++ TWIST.Server/Hubs/RoomHub.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using TWISTServer.Controllers;
 using TWISTServer.DatabaseComponents.DataAccessors;
 using TWISTServer.DatabaseComponents.Records;
+using TWISTServer.Enums;
 
 namespace TWISTServer.Hubs
 {
@@ -18,17 +19,54 @@ namespace TWISTServer.Hubs
     {
         SimulationsDataAccessor simAccessor = new();
         ParticipantsDataAccessor partAccessor = new();
-        public async Task JoinRoom(string code, string username, string email) // FYI: Methods like this will SILENTLY FAIL if you do not pass the correct param types
+        public async Task JoinRoom(SimulationRecord sim, string username, string email) // FYI: Methods like this will SILENTLY FAIL if you do not pass the correct param types
         {
-            /// TODO: Check if the email address already exists
-            
-            // Get simulation
-            List<SimulationRecord> sims = simAccessor.GetByCode(code).ToList();
-
-            if (sims.Count == 0)
+            // Check if the email address does not already exist in sim
+            var _ = partAccessor.GetParticipantFromSimulationAndEmail(sim.SimulationId, email);
+            ParticipantRecord newParticipant;
+            if (_.Count() <= 0)
             {
-                throw new HubException("Simulation not found");
+                // Add the participant to participants table
+                newParticipant = new ParticipantRecord(0, CountryEnum.None, ParticipantRoleEnum.None, sim.SimulationId, username, email);
+                int participantId = partAccessor.InsertAndReturnId(newParticipant);
+
+                // Add participant to simulation record
+                var newParticipants = sim.Participants.Append(participantId);
+                simAccessor.UpdateParticipants(sim.SimulationId, newParticipants);
             }
+
+            newParticipant = partAccessor.GetParticipantFromSimulationAndEmail(sim.SimulationId, email).First();
+
+            // Add the participant to the main group
+            await Groups.AddToGroupAsync(Context.ConnectionId, sim.Code);
+
+            // Add the participant to default country
+            string teamName = $"{sim.Code}_{newParticipant.Country}";
+            await Groups.AddToGroupAsync(Context.ConnectionId, teamName);
+
+            // Send signal to all clients
+            await Clients.All.ParticipantJoined(newParticipant);
+        }
+
+        public async Task LeaveRoom(SimulationRecord sim, ParticipantRecord participant)
+        {
+            // Remove the participant from the participants table
+            partAccessor.Delete(participant.ParticipantId);
+
+            // Remove participant from simulation record
+            var newParticipants = sim.Participants.Where(p => p != participant.ParticipantId);
+            simAccessor.UpdateParticipants(sim.SimulationId, newParticipants);
+
+            // Remove the participant from main group
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, sim.Code);
+
+            // Remove the participant from team
+            string teamName = $"{sim.Code}_{participant.Country}";
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, teamName);
+
+            // Send signal to all in simulation
+            await Clients.Group(sim.Code).ParticipantLeft(participant.ParticipantId, participant.Email, participant.Username);
+        }
 
         public async Task KickParticipant(SimulationRecord sim, ParticipantRecord participant)
         {
