b724acdf335175d9e826e9f313eb23e539eb7db8
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-01-14 14:48:15 -0600

    change into table type

--- twist.client/src/components/ParticipantList/ParticipantList.tsx
+++ twist.client/src/components/ParticipantList/ParticipantList.tsx
@@ -1,7 +1,8 @@
-import CountryEnum from '../../enums/CountryEnum';
-import RoleEnum from '../../enums/RoleEnum';
 import ParticipantModel from '../../models/ParticipantModel';
 import ParticipantListItem from './ParticipantListItem';
+import CountryEnum from '../../enums/CountryEnum';
+import RoleEnum from '../../enums/RoleEnum';
+import { Table } from "react-bootstrap";
 
 type ParticipantListProps = {
     participants: ParticipantModel[];
@@ -10,19 +11,37 @@ type ParticipantListProps = {
     onRoleChanged: (participant: ParticipantModel, role: RoleEnum) => void;
 };
 
-const ParticipantList: React.FC<ParticipantListProps> = ({ participants, onKickClicked, onCountryChanged, onRoleChanged }) => {
-
+const ParticipantList: React.FC<ParticipantListProps> = ({
+    participants,
+    onKickClicked,
+    onCountryChanged,
+    onRoleChanged
+}) => {
     return (
-        <div>
-            {participants.map((p, i) => (<ParticipantListItem
-                key={i}
-                participant={p}
-                onKickClicked={() => onKickClicked(p, i)}
-                onCountryChanged={(country) => onCountryChanged(p, country)}
-                onRoleChanged={(role) => onRoleChanged(p, role)}
-            />))}
-        </div>
+        <Table className="participant-list">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Country</th>
+                    <th>Role</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {participants.map((p, i) => (
+                    <ParticipantListItem
+                        key={p.participant_id} // or you can still use `i` if IDs aren't guaranteed unique
+                        participant={p}
+                        onKickClicked={() => onKickClicked(p, i)}
+                        onCountryChanged={(country) => onCountryChanged(p, country)}
+                        onRoleChanged={(role) => onRoleChanged(p, role)}
+                    />
+                ))}
+            </tbody>
+        </Table>
     );
-}
+};
 
 export default ParticipantList;
--- twist.client/src/components/ParticipantList/ParticipantListItem.tsx
+++ twist.client/src/components/ParticipantList/ParticipantListItem.tsx
@@ -1,6 +1,5 @@
 import { Button, Dropdown } from 'react-bootstrap';
 import type ParticipantModel from '../../models/ParticipantModel';
-import "./style.scss";
 import CountryEnum, { countries, CountryStringMap, StringCountryMap } from '../../enums/CountryEnum';
 import RoleEnum, { roles, RoleStringMap, StringRoleMap } from '../../enums/RoleEnum';
 
@@ -11,44 +10,57 @@ type ParticipantListItemProps = {
     onRoleChanged: (role: RoleEnum) => void;
 };
 
-const ParticipantListItem: React.FC<ParticipantListItemProps> = ({ participant, onKickClicked, onCountryChanged, onRoleChanged }) => {
+const ParticipantListItem: React.FC<ParticipantListItemProps> = ({
+    participant,
+    onKickClicked,
+    onCountryChanged,
+    onRoleChanged
+}) => {
 
     return (
-        <div className="participantListItem" >
-            <p>{participant.participant_id}</p>
-            <p>{participant.username}</p>
-            <p>{participant.email}</p>
-            <Dropdown>
-                <Dropdown.Toggle>
-                    {CountryStringMap.get(participant.country ?? 0)}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {countries.map((c: string, i) => (
-                        <Dropdown.Item
-                            key={i}
-                            eventKey={c}
-                            onClick={() => onCountryChanged(StringCountryMap.get(c)!)}>
-                            {c}
-                        </Dropdown.Item>))}
-                </Dropdown.Menu>
-            </Dropdown>
-            <Dropdown>
-                <Dropdown.Toggle>
-                    {RoleStringMap.get(participant.role ?? 0)}
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                    {roles.map((r, i) => (
-                        <Dropdown.Item
-                            key={i}
-                            eventKey={r}
-                            onClick={() => onRoleChanged(StringRoleMap.get(r)!)}>
-                            {r}
-                        </Dropdown.Item>))}
-                </Dropdown.Menu>
-            </Dropdown>
-            <Button onClick={onKickClicked}>Kick</Button>
-        </div>
+        <tr className="participant-list-item">
+            <td>{participant.participant_id}</td>
+            <td>{participant.username}</td>
+            <td>{participant.email}</td>
+            <td>
+                <Dropdown>
+                    <Dropdown.Toggle>
+                        {CountryStringMap.get(participant.country ?? 0)}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {countries.map((c: string, i) => (
+                            <Dropdown.Item
+                                key={i}
+                                onClick={() => onCountryChanged(StringCountryMap.get(c)!)}
+                            >
+                                {c}
+                            </Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </td>
+            <td>
+                <Dropdown>
+                    <Dropdown.Toggle>
+                        {RoleStringMap.get(participant.role ?? 0)}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {roles.map((r, i) => (
+                            <Dropdown.Item
+                                key={i}
+                                onClick={() => onRoleChanged(StringRoleMap.get(r)!)}
+                            >
+                                {r}
+                            </Dropdown.Item>
+                        ))}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </td>
+            <td>
+                <Button onClick={onKickClicked}>Kick</Button>
+            </td>
+        </tr>
     );
-}
+};
 
 export default ParticipantListItem;
