7416d19a281921209caa1695fea9c3325978d5a1
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-01-16 13:28:54 -0600

    add strong typing

--- twist.client/src/signalr/useRoomHub.ts
+++ twist.client/src/signalr/useRoomHub.ts
@@ -1,4 +1,4 @@
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from "@microsoft/signalr";
 import { useState, useEffect } from "react";
 import { SERVER_URL } from "../server/server_consts";
 
@@ -14,14 +14,14 @@ export function useRoomHub(simCode: string) {
         setConnection(conn);
 
         return () => {
-            // We don't stop here. We'll stop in the second effect's cleanup.
+            // Don't stop here
         };
     }, [simCode]);
 
     useEffect(() => {
         if (!connection) return;
 
-        if (connection.state !== "Disconnected") {
+        if (connection.state !== HubConnectionState.Disconnected) {
             console.error("Cannot start connection when it is not in the Disconnected state.");
             return;
         }
